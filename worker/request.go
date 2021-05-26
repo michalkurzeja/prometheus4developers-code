782111b12340c11049b3fd9536de7e5b03824628
michalkurzeja <mike.kurzeja@gmail.com>
2021-05-26 07:52:02 +0200

    better response stats

--- worker/request.go
+++ worker/request.go
@@ -1,6 +1,8 @@
 package worker
 
 import (
+	"math/rand"
+	"net/http"
 	"time"
 
 	"github.com/brainly/prometheus-presentation/randx"
@@ -9,7 +11,7 @@ import (
 /**
 Measure:
 - response time
-- request rate
+- request rate (by status)
 - number of concurrent requests
 */
 
@@ -21,4 +23,16 @@ func NewRequestHandler() *RequestHandler {
 
 func (h *RequestHandler) Work() {
 	time.Sleep(randx.Duration(time.Millisecond, 300*time.Millisecond))
+	status := h.randomStatus()
+	_ = status
+}
+
+// randomStatus returns a random status code.
+// There's approx. 5% chance that the code will be "400", "200" otherwise.
+func (h RequestHandler) randomStatus() int {
+	if rand.Float32() < 0.05 {
+		return http.StatusBadRequest
+	}
+
+	return http.StatusOK
 }
